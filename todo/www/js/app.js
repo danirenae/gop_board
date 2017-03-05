5c95ede74039690264885d36dc119a7aadd53f6e
Dani Adkins <danirenaeadkins12@gmail.com>
2017-03-05 14:00:10 -0600

    completed ionic tutorial

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -5,18 +5,6 @@
 // the 2nd parameter is an array of 'requires'
 // angular.module('starter', ['ionic'])
 
-
-// angular.module('todo', ['ionic'])
-
-// .controller('TodoCtrl', function($scope) {
-//   $scope.tasks = [
-//     { title: 'Collect coins' },
-//     { title: 'Eat mushrooms' },
-//     { title: 'Get high enough to grab the flag' },
-//     { title: 'Find the Princess' }
-//   ];
-// })
-
 angular.module('todo', ['ionic'])
 
 .controller('TodoCtrl', function($scope, $ionicModal) {
@@ -168,23 +156,3 @@ angular.module('todo', ['ionic'])
   }, 1000);
 
 })
-
-
-
-// .run(function($ionicPlatform) {
-//   $ionicPlatform.ready(function() {
-//     if(window.cordova && window.cordova.plugins.Keyboard) {
-//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//       // for form inputs)
-//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-//       // Don't remove this line unless you know what you are doing. It stops the viewport
-//       // from snapping when text inputs are focused. Ionic handles this internally for
-//       // a much nicer keyboard experience.
-//       cordova.plugins.Keyboard.disableScroll(true);
-//     }
-//     if(window.StatusBar) {
-//       StatusBar.styleDefault();
-//     }
-//   });
-// })
